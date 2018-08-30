3c911e67d2a168a44d6fe1ef80d4ebc322389ab6
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-30 18:57:14 +0000

    complete assignment

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
-  }
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire !== original) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
+  } 
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else if (this.numberOfVampiresFromOriginal === vampire.numberOfVampiresFromOriginal) {
+      return "they are the same distance from the original vampire";
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -39,6 +51,26 @@ class Vampire {
 
   }
 }
+const original = new Vampire('Original', 956);
+const ansel = new Vampire('Ansel', 1225);
+const bart = new Vampire('Bart', 1211);
+const elgort = new Vampire('Elgort', 1253);
+const sarah = new Vampire('Sarah', 1257);
+const andrew = new Vampire('Andrew', 1344);
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+elgort.addOffspring(andrew);
+
+console.log(original.isMoreSeniorThan(ansel))
+
+
+
+
+
+
 
 module.exports = Vampire;
 
